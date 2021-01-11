ef7edde81e4c6739075dc46891ac51a46541dce2
Muhammed Oguz <muhammed0748@outlook.com>
2021-01-12 01:11:52 +0300

    HexAdapter Class finished

--- include/HexAdapter.h
+++ include/HexAdapter.h
@@ -7,30 +7,56 @@
 
 #ifndef HEXADAPTER_H
 #define HEXADAPTER_H
+
 #include "AbstractHex.h"
-#include "Cell.h"
+#include <vector>
+#include <iostream>
+#include <ostream>
+#include <fstream>
+
+using std::string;
 
 
 
 namespace HexOguz
 {
-    template <typename T>
-    class HexAdapter
+    template<template<typename...> class T>
+    class HexAdapter: public AbstractHex
     {
     public:
         HexAdapter();
         HexAdapter(int);
 
-        void print() const;
-        void reset();
-        void createTable();
+        void playGame();
+        
+        void print() const override;
+        void readFromFile(string) override;
+        void writeToFile(string) override;
+        void reset() override;
+        void setSize(int) override;
+        void play() override; 
+        void play(Cell) override; 
+        Cell operator()(int,int) const override;
+
 
     private:
         T<T<Cell>> hexCells;
+        T<T<Cell>> keepUppercase;
+        // Check winner Helper Functions.
+        bool checkWinner();
+        bool check(int,int);
+        bool isValid(int,int);
+
+        // After game is ended. Convert Winning path to UpperCase
+        void copyToUpper();
+
+        // check entered command
+        bool checkCommand(string, Cell&);
+
+        // create empty table
+        void createTable();
 
     };
-    
-    
 
 } // namespace HexOguz
 
